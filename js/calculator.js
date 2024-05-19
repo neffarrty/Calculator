d04e0c7b6085cae6bdde38f4edf426dd27c743f3
neffarrty <egorkovtun8@gmail.com>
2024-05-19 18:43:03 +0300

    make display logic

--- js/calculator.js
+++ js/calculator.js
@@ -1,24 +1,75 @@
-const flip_btn = document.getElementById('flip-btn');
-const flip = document.getElementById('flip');
-const front = document.getElementById('calculator');
-const back = document.getElementById('bulbulator');
-let cnt = 0;
+const previous = document.getElementById('previous');
+const current = document.getElementById('current');
+const digitButtons = document.querySelectorAll('[data-digit]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const negateButton = document.querySelector('[data-negate]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+let expression = ['0'];
+let memory = 0;
+let result;
 
-flip_btn.addEventListener('click', () => {
-    if(cnt === 0){
-        flip.style.transform = "rotateY(180deg)";
-        setTimeout(() =>{
-            back.style.display = "grid";
-            front.style.display = "none";
-        },250);
-        cnt = 1;
+digitButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (expression.length === 1 && result !== undefined) {
+            expression[expression.length - 1] = button.innerText;
+            current.textContent = expression.join(' ');
+        }
+        else if (expression[expression.length - 1] === '0') {
+            console.log('0');
+            expression[expression.length - 1] = button.innerText;
+            current.textContent = expression.join(' ');
+        }
+        else {
+            current.textContent = current.textContent + button.innerText;
+            expression = current.textContent.trim().split(' ');
+        }
+    });
+});
+
+operationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (expression[expression.length - 1].match(/^[^0-9]$/)) {
+            expression[expression.length - 1] = button.innerText;
+            current.textContent = expression.join(' ') + ' ';
+        }
+        else {
+            current.textContent = current.textContent + ` ${button.innerText} `;
+            expression = current.textContent.trim().split(' ');
+        }
+    });
+});
+
+equalsButton.addEventListener('click', (button) => {
+    const func = new Function(`return ` + expression.join(' '));
+    result = func();
+    previous.textContent = current.textContent;
+    current.textContent = result;
+    expression = [`${result}`];
+});
+
+clearButton.addEventListener('click', () => {
+    previous.textContent = '';
+    current.textContent = '0';
+    expression = ['0'];
+    result = undefined;
+});
+
+deleteButton.addEventListener('click', () => {
+    if (expression.length === 1) {
+        expression = ['0'];
     }
-    else{
-        flip.style.transform = "rotateY(0deg)";
-        setTimeout(() =>{
-            front.style.display = "grid";
-            back.style.display = "none";
-        },250);
-        cnt = 0;
+    else {
+        expression.splice(expression.length - 1, 1);
     }
+    current.textContent = expression.join(' ');
+});
+
+negateButton.addEventListener('click', (button) => {
+    let number = parseFloat(expression[expression.length - 1]);
+    if (!Number.isNaN(number)) {
+        expression[expression.length - 1] = String(-number);
+    }
+    current.textContent = expression.join(' ');
 });
