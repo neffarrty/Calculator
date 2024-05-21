467534968f49730ab3c5801b5d21709b37f15097
neffarrty <egorkovtun8@gmail.com>
2024-05-21 19:57:44 +0300

    fix calculation

--- js/calculator.js
+++ js/calculator.js
@@ -1,23 +1,85 @@
 const previous = document.getElementById('previous');
 const current = document.getElementById('current');
-const digitButtons = document.querySelectorAll('[data-digit]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const negateButton = document.querySelector('[data-negate]');
-const equalsButton = document.querySelector('[data-equals]');
-const clearButton = document.querySelector('[data-clear]');
-const deleteButton = document.querySelector('[data-delete]');
 let expression = ['0'];
-let memory = 0;
-let result;
 
-digitButtons.forEach((button) => {
+let memory = 0;
+let result = null;
+
+function factorial(num) {
+    if (num === 0) { 
+        return 1;
+    }
+    else { 
+        return num * factorial(num - 1); 
+    }
+}
+
+function calculate(expression) {
+    let copy = expression.map((n) => {
+        let a = parseFloat(n);
+        return Number.isNaN(a) ? n : a;
+    });
+
+    if (Number.isNaN(parseFloat(copy[copy.length - 1]))) {
+        copy.splice(copy.length - 1, 1);
+    }
+
+    while (copy.includes('*')) {
+        let i = copy.indexOf('*');
+
+        if (copy.includes('/') && copy.indexOf('/') < copy.indexOf('*')) {
+            i = copy.indexOf('/');
+            copy.splice(i - 1, 3, copy[i - 1] / copy[i + 1]);
+            continue;
+        }
+        copy.splice(i - 1, 3, copy[i - 1] * copy[i + 1]);
+    }
+
+    while (copy.includes('/')) {
+        let i = copy.indexOf('/');
+
+        if (copy.includes('*') && copy.indexOf('*') < copy.indexOf('/')) {
+            i = copy.indexOf('*');
+            copy.splice(i - 1, 3, copy[i - 1] * copy[i + 1]);
+            continue;
+        }
+        copy.splice(i - 1, 3, copy[i - 1] / copy[i + 1]);
+    }
+
+    while (copy.includes('+')) {
+        let i = copy.indexOf('+');
+
+        if (copy.includes('-') && copy.indexOf('-') < copy.indexOf('+')) {
+            i = copy.indexOf('-');
+            copy.splice(i - 1, 3, copy[i - 1] - copy[i + 1]);
+            continue;
+        }
+        copy.splice(i - 1, 3, copy[i - 1] + copy[i + 1]);
+    }
+
+    while (copy.includes('-')) {
+        let i = copy.indexOf('-');
+
+        if (copy.includes('+') && copy.indexOf('+') < copy.indexOf('-')) {
+            i = copy.indexOf('+');
+            copy.splice(i - 1, 3, copy[i - 1] + copy[i + 1]);
+            continue;
+        }
+        copy.splice(i - 1, 3, copy[i - 1] - copy[i + 1]);
+    }
+
+    return copy[0];
+}
+
+
+document.querySelectorAll('[data-digit]').forEach((button) => {
     button.addEventListener('click', () => {
-        if (expression.length === 1 && result !== undefined) {
+        if (expression.length === 1 && result !== null) {
+            result = null;
             expression[expression.length - 1] = button.innerText;
             current.textContent = expression.join(' ');
         }
         else if (expression[expression.length - 1] === '0') {
-            console.log('0');
             expression[expression.length - 1] = button.innerText;
             current.textContent = expression.join(' ');
         }
@@ -28,7 +90,7 @@ digitButtons.forEach((button) => {
     });
 });
 
-operationButtons.forEach((button) => {
+document.querySelectorAll('[data-operation]').forEach((button) => {
     button.addEventListener('click', () => {
         if (expression[expression.length - 1].match(/^[^0-9]$/)) {
             expression[expression.length - 1] = button.innerText;
@@ -41,22 +103,68 @@ operationButtons.forEach((button) => {
     });
 });
 
-equalsButton.addEventListener('click', (button) => {
-    const func = new Function(`return ` + expression.join(' '));
-    result = func();
+document.querySelector('[data-percent').addEventListener('click', () => {
+    if (expression.length > 2) {
+        let last = parseFloat(expression[expression.length - 1]);
+    
+        if (!Number.isNaN(last)) {
+            expression[expression.length - 1] = (expression[expression.length - 3] / 100 * last).toFixed(2);
+            current.textContent = expression.join(' ');
+        }
+    }
+});
+
+document.querySelector('[data-factorial').addEventListener('click', () => {
+    let last = parseInt(expression[expression.length - 1]);
+
+    if (!Number.isNaN(last)) {
+        expression[expression.length - 1] = factorial(last);
+        current.textContent = expression.join(' ');
+    }
+});
+
+document.querySelector('[data-sqrt').addEventListener('click', () => {
+    let last = parseFloat(expression[expression.length - 1]);
+    
+    if (!Number.isNaN(last)) {
+        expression[expression.length - 1] = Math.sqrt(last).toFixed(2);
+        current.textContent = expression.join(' ');
+    }
+});
+
+document.querySelector('[data-sqr').addEventListener('click', () => {
+    let last = parseFloat(expression[expression.length - 1]);
+    
+    if (!Number.isNaN(last)) {
+        expression[expression.length - 1] = (last * last).toFixed(2);
+        current.textContent = expression.join(' ');
+    }
+});
+
+document.querySelector('[data-cbr').addEventListener('click', () => {
+    let last = parseFloat(expression[expression.length - 1]);
+    
+    if (!Number.isNaN(last)) {
+        expression[expression.length - 1] = (last * last * last).toFixed(2);
+        current.textContent = expression.join(' ');
+    }
+});
+
+document.querySelector('[data-equals]').addEventListener('click', () => {
+    result = calculate(expression);
     previous.textContent = current.textContent;
     current.textContent = result;
     expression = [`${result}`];
 });
 
-clearButton.addEventListener('click', () => {
+document.querySelector('[data-clear]').addEventListener('click', () => {
     previous.textContent = '';
     current.textContent = '0';
     expression = ['0'];
-    result = undefined;
+    result = null;
 });
 
-deleteButton.addEventListener('click', () => {
+document.querySelector('[data-delete]').addEventListener('click', () => {
     if (expression.length === 1) {
         expression = ['0'];
     }
@@ -66,7 +174,7 @@ deleteButton.addEventListener('click', () => {
     current.textContent = expression.join(' ');
 });
 
-negateButton.addEventListener('click', (button) => {
+document.querySelector('[data-negate]').addEventListener('click', () => {
     let number = parseFloat(expression[expression.length - 1]);
     if (!Number.isNaN(number)) {
         expression[expression.length - 1] = String(-number);
